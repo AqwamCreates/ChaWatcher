b2e44ec84493d6dadf640e0cfb39870fa2939056
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-10-11 22:43:31 +0800

    Create ModelCreator.lua

---
+++ src/Custom/ModelCreator.lua
@@ -0,0 +1,142 @@
+local DataStoreService = game:GetService("DataStoreService")
+
+local DataCollectorDataStore = DataStoreService:GetDataStore("AqwamChaWatcherDataCollectorDataStore")
+
+local AnomalyDetectorDataStore = DataStoreService:GetDataStore("AqwamChaWatcherAnomalyDetectorDataStore")
+
+ModelCreator = {}
+
+ModelCreator.__index = ModelCreator
+
+local ChaWatcher = script.Parent.Parent.Parent
+
+local function createSupportVectorMachine()
+	
+	local SupportVectorMachine = require(ChaWatcher.AqwamProprietarySourceCodes.SupportVectorMachine).new()
+	
+	SupportVectorMachine:setWaitDurations(0.3)
+	
+	return SupportVectorMachine
+	
+end
+
+function ModelCreator.new(useOnlineData: boolean, dataCollectorDataStoreKey: string, anomalyDetectorDataStoreKey: string)
+	
+	local NewModelCreator = {}
+	
+	setmetatable(NewModelCreator, ModelCreator)
+	
+	dataCollectorDataStoreKey = dataCollectorDataStoreKey or "default"
+	
+	anomalyDetectorDataStoreKey = anomalyDetectorDataStoreKey or "default"
+
+	if (typeof(dataCollectorDataStoreKey) ~= "string") then error("Data collector datastore key is not a string value!") end
+	
+	if (typeof(anomalyDetectorDataStoreKey) ~= "string") then error("Anomaly detector datastore key is not a string value!") end
+	
+	NewModelCreator.UseOnlineData = useOnlineData
+	
+	NewModelCreator.DataCollectorDataStoreKey = dataCollectorDataStoreKey
+	
+	NewModelCreator.AnomalyDetectorDataStoreKey = dataCollectorDataStoreKey
+	
+	NewModelCreator.Model = {}
+	
+	NewModelCreator.SupportVectorMachine = createSupportVectorMachine()
+	
+	return NewModelCreator
+	
+end
+
+function ModelCreator:setParameters(maxNumberOfIterations, cValue, targetCost, kernelFunction, kernelParameters)
+	
+	self.Model = {}
+	
+	self.Model.kernelFunction = kernelFunction
+	self.Model.kernelParameters = kernelParameters
+	
+	self.SupportVectorMachine:setParameters(maxNumberOfIterations, cValue, targetCost, kernelFunction, kernelParameters)
+	
+end
+
+local function fetchData(useOnlineData: boolean, key: string)
+
+	if useOnlineData then
+
+		return DataCollectorDataStore:GetAsync(key)
+
+	else
+
+		return require(script.Parent.OfflineData)[key]
+
+	end
+
+end
+
+function ModelCreator:saveModelOnline()
+	
+	if not self.AnomalyDetectorDataStoreKey then return false end
+
+	local success
+
+	repeat
+
+		success = pcall(function()
+
+			AnomalyDetectorDataStore:SetAsync(self.AnomalyDetectorDataStoreKey, self.Model)
+
+		end)
+
+		task.wait(0.1)
+
+	until success
+
+	print("Model has been saved!")
+
+	return true
+	
+end
+
+function ModelCreator:train(numberOfDataToUse: number)
+	
+	local data = fetchData(self.UseOnlineData, self.DataCollectorDataStoreKey)
+	
+	if not data then error("No data!") end
+	
+	if (numberOfDataToUse > #data) then error("Number of data to use exceeds the number of data!") end
+	
+	while (#data > numberOfDataToUse) do
+		
+		local randomIndex = Random.new():NextInteger(1, #data)
+		
+		table.remove(data, randomIndex)
+		
+	end
+	
+	task.wait(0.3)
+	
+	local labels = {}
+	
+	for i = 1, #data, 1 do
+		
+		table.insert(labels, {1})
+		
+	end
+	
+	task.wait(0.3)
+	
+	self.SupportVectorMachine:train(data, labels)
+	
+	self.Model.ModelParameters = self.SupportVectorMachine:getModelParameters()
+	
+	return self.Model
+	
+end
+
+function ModelCreator:getModel()
+	
+	return self.Model
+	
+end
+
+return ModelCreator

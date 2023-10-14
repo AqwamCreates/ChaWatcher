8a1fd4277fb5c66c8355e2e245c943d9ed026f03
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-10-14 20:38:22 +0800

    Create DataCollection_DistributedComputing.lua

---
+++ docs/ExampleCodes/DataCollection_DistributedComputing.lua
@@ -0,0 +1,42 @@
+# Data Collection (Original Version)
+
+```lua
+
+local ServerScriptService = game:GetService("ServerScriptService")
+
+local MatrixL = require(ServerScriptService.MatrixL)
+
+local ChaWatcher = require(ServerScriptService.ChaWatcher)
+
+local DataCollector = ChaWatcher.Original.DataCollector.new(true, "1")
+
+DataCollector:bindToHeartbeat(function(Player, fullDataVector) -- Runs a function on every heartbeat.
+
+	print(Player.Name .. "\'s data has been collected!")
+
+	local distance = fullDataVector[14]
+
+end)
+
+DataCollector:bindToMissingData(function(Player) -- Runs a function if cannot create a data vector.
+
+	print(Player.Name .. " has missing data!")
+
+	local currentDataVector, previousDataVector = DataCollector:getPlayerDataVectors()
+
+end)
+
+DataCollector:start() -- Starts collecting data.
+DataCollector:stop()  -- Stops collecting data.
+DataCollector:start() -- Starts collecting data. Again!
+
+game:BindToClose(function()
+
+	local fullData = DataCollector:getFullData()
+	MatrixL:printMatrixWithComma(fullData)
+
+	DataCollector:saveFullDataOnline() -- Saves data to online
+
+end)
+
+```
